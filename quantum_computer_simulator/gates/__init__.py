956b32d04f5d964d84dedb340ef740a37bfcc808
johnyob <johnyob132@gmail.com>
2018-07-27 17:19:10 +0100

    Create quantum_computer_simulator.gates package.

---
+++ quantum_computer_simulator/gates/__init__.py
@@ -0,0 +1,7 @@
+from quantum_computer_simulator.gates.H import H
+from quantum_computer_simulator.gates.X import X
+from quantum_computer_simulator.gates.Y import Y
+from quantum_computer_simulator.gates.Z import Z
+from quantum_computer_simulator.gates.R import R
+from quantum_computer_simulator.gates.SqrtNOT import SqrtNOT
+from quantum_computer_simulator.gates.CNOT import CNOT
