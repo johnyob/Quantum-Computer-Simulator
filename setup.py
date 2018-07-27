57d890b6cc0850954e9fbf70f40c291a41f74331
johnyob <johnyob132@gmail.com>
2018-07-27 17:20:17 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+requires = [
+    "numpy>=1.14.5"
+]
+
+packages = [
+    "quantum_computer_simulator"
+]
+
+setuptools.setup(
+    name="quantum-computer-simulator",
+    version="0.0.1",
+    author="Alistair O'Brien",
+    author_email="alistair.o'brien@ellesmere.com",
+    description="A Quantum Computer Simulator written in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnyob/Quantum-Computer-Simulator",
+    packages=packages,
+    install_requires=requires
+)
